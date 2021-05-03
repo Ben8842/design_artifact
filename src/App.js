617d90b648277643dd6625618a2e8cdf17291056
Ben8842 <z87e99b44r55a@gmail.com>
2021-05-03 12:50:28 -0400

    added component and import and img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World<div>hello again</div>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: false,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World<div>hello again</div>
+      </div>
+    );
+  }
 }
 
 export default App;
